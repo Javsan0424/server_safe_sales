7714cc4f9cbbea189f2b4d1bc4ce343d17d96028
Javsan0424 <A01198909@tec.mx>
2025-04-06 20:47:01 -0600

    Delete origin

--- index.ts
+++ index.ts
@@ -5,7 +5,6 @@ import dataRoutes from './src/routes/data';
 const app = express();
 
 app.use(cors({
-    origin: 'https://client-safe-sales-2.vercel.app', 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
